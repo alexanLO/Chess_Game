3031f57cbee1a03e6a65d973a750d1a6ae8d64bb
Alexan Lima <alexan.works@gmail.com>
2022-01-06 13:02:58 -0300

    Começando a implementar Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
+        Board board = new Board(3, 4);
         
     }
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -26,4 +26,9 @@ public class Position {
     public void setRow(int row) {
         this.row = row;
     }
+
+    @Override 
+    public String toString(){
+        return row + ", " + column;
+    }
 }
