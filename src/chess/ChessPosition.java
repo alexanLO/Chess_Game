08dbe35212c031980424799a43208671b4ee84e0
Alexan Lima <alexan.works@gmail.com>
2022-01-07 15:30:29 -0300

    Adicionando ChessException e ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,26 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(int row, char column){
+        this.row = row;
+        this.column = column;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected ChessPosition fromPosition(Position position){
+        return new ChessPosition(8- position.getRow(), (char) ('a' - position.getColumn()));
+    }
+
+    @Override
+    public String toString(){
+        return "" + column + row;
+    }
+}
